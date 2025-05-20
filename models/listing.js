1a2078f95a1d5d56260411cc7cd7f22de4d04dae
Om Pradnya <294ompednekar@gmail.com>
2025-05-20 22:10:34 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,47 +1,3 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-// const Review = require("./review.js");
-
-// const listingSchema = new Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   description: String,
-//   image: {
-//     filename: String,
-//     url: {
-//       type: String,
-//       default:
-//         "https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//     }, //fix
-//     // set: (v) =>
-//     //   v === ""
-//     //     ? "https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-//     //     : v,
-//   },
-//   price: Number,
-//   location: String,
-//   country: String,
-//   reviews: [
-//     {
-//       type: Schema.Types.ObjectId,
-//       ref: "Review",
-//     },
-//   ],
-// });
-// //--------mongoose mw for delete propagtion (reviews) ------------
-// listingSchema.post("findOneAndDelete", async (listing) => {
-//   if (listing) {
-//     await Review.deleteMany({ _id: { $in: listing.reviews } });
-//   }
-// });
-// //-------------------------------------------------------------
-// const Listing = mongoose.model("Listing", listingSchema);
-// module.exports = Listing;
-
-//----------------TA
-
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
@@ -52,21 +8,6 @@ const listingSchema = new Schema({
     required: true,
   },
   description: String,
-  //------------logic for URL upload ----------------
-  // image: {
-  //   filename: String,
-  //   url: {
-  //     type: String,
-  //     default:
-  //       "https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //     //fix
-  //     set: (v) =>
-  //       v === ""
-  //         ? "https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  //         : v,
-  //   },
-  // },-----------------------------------------------
-
   image: {
     url: String,
     filename: String,
@@ -86,10 +27,6 @@ const listingSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
-  // category: {
-  //   type:String,
-  //   enum: ["mountains","camps","boats","yachts", ]
-  // }
 });
 //--------mongoose mw for delete propagtion (reviews) ------------
 listingSchema.post("findOneAndDelete", async (listing) => {
